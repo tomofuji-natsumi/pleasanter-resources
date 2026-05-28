319b364f151a735e166ffe9b480925ef30a5f6d9
友藤なつみ <natsumi.tomofuji@proassist.co.jp>
2026-05-28 12:08:31 +0900

    Update comment_color.js

--- js/sites/comment_color.js
+++ js/sites/comment_color.js
@@ -1,10 +1,8 @@
 /* コード元作成者：植盛さん */
-$p.events.on_editor_load_arr = [];
-
-$p.events.on_editor_load_arr.push(function() {
+function applyCommentColors() {
 
     if (!$("#MyComments")[0]) return;
-    
+
     let myData = JSON.parse($('#MyComments').val());    
     let myKeys = Object.keys(myData);
     myKeys.forEach(function (myKey) {
@@ -17,10 +15,23 @@ $p.events.on_editor_load_arr.push(function() {
             });
         }
     });
-})
-
-$p.events.on_editor_load = function() {
-    for (let i = 0; i < $p.events.on_editor_load_arr.length; i++) {
-        $p.events.on_editor_load_arr[i] ();
-    }
 }
+
+// コメント DOM の変化を監視
+const commentObserver = new MutationObserver(mutations => {
+    for (const m of mutations) {
+        for (const node of m.addedNodes) {
+
+            // コメントが追加された瞬間に色付け
+            if (node.nodeType === 1 && node.classList.contains("comment")) {
+                applyCommentColors();
+            }
+        }
+    }
+});
+
+// コメント一覧を監視
+commentObserver.observe(document.body, {
+    childList: true,
+    subtree: true
+});
