1fa311c9634dd625eee23abf5af947bebf3dc449
友藤なつみ <natsumi.tomofuji@proassist.co.jp>
2026-06-04 15:19:41 +0900

    Update loader_sites_kari.js

--- js/loader_sites_kari.js
+++ js/loader_sites_kari.js
@@ -1,31 +1,44 @@
-const scripts = [
-  "https://tomofuji-natsumi.github.io/pleasanter-resources/js/port_setting_kari.js",
-  "https://tomofuji-natsumi.github.io/pleasanter-resources/js/sites/readonly.js",
+const criticalScripts = [
+  "https://tomofuji-natsumi.github.io/pleasanter-resources/js/common/icon.js"
+];
+
+const dependentScripts = [
+  "https://tomofuji-natsumi.github.io/pleasanter-resources/js/port_setting_kari2.js",
+  "https://tomofuji-natsumi.github.io/pleasanter-resources/js/sites/readonly.js"
+];
+
+const parallelScripts = [
   "https://tomofuji-natsumi.github.io/pleasanter-resources/js/sites/comment_color.js",
-  "https://tomofuji-natsumi.github.io/pleasanter-resources/js/sites/holiday_setting.js",
-  "https://tomofuji-natsumi.github.io/pleasanter-resources/js/common/icon.js",
+  "https://tomofuji-natsumi.github.io/pleasanter-resources/js/sites/holiday_setting.js"
 ];
 
 function loadScriptOnce(url) {
   return new Promise((resolve, reject) => {
-    if (document.querySelector('script[src="' + url + '"]')) {
-      return resolve();
-    }
+    if (document.querySelector(`script[src="${url}"]`)) return resolve();
+
     const s = document.createElement('script');
     s.src = url;
-    s.async = false;
+    s.async = true; // ← 並列ロードを有効化
     s.onload = resolve;
     s.onerror = reject;
     document.head.appendChild(s);
   });
 }
 
-function loadScriptsSequential(urls) {
-  return urls.reduce((p, url) => {
-    return p.then(() => loadScriptOnce(url));
-  }, Promise.resolve());
+async function loadSequential(urls) {
+  for (const url of urls) {
+    await loadScriptOnce(url);
+  }
 }
 
-window.runTenantScripts = function () {
-  return loadScriptsSequential(scripts);
+window.runTenantScripts = async function () {
+
+  // ① クリティカル（最初に読み込む）
+  await loadSequential(criticalScripts);
+
+  // ② 依存関係のあるもの（直列）
+  await loadSequential(dependentScripts);
+
+  // ③ 依存のないもの（並列）
+  await Promise.all(parallelScripts.map(loadScriptOnce));
 };
