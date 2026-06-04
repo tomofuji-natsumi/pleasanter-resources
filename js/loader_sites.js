a4a79f7453711b1896ce5812a8050f8891d43a7e
友藤なつみ <natsumi.tomofuji@proassist.co.jp>
2026-06-04 14:50:05 +0900

    Update loader_sites.js

--- js/loader_sites.js
+++ js/loader_sites.js
@@ -1,8 +1,9 @@
 // 複数JSをまとめて読み込む
 const scripts = [
   "https://tomofuji-natsumi.github.io/pleasanter-resources/js/port_setting_kari.js",
-  "https://tomofuji-natsumi.github.io/pleasanter-resources/js/sites/comment_color.js",
   "https://tomofuji-natsumi.github.io/pleasanter-resources/js/sites/readonly.js",
+  "https://tomofuji-natsumi.github.io/pleasanter-resources/js/sites/comment_color.js",
+  "https://tomofuji-natsumi.github.io/pleasanter-resources/js/sites/holiday_setting.js",
   "https://tomofuji-natsumi.github.io/pleasanter-resources/js/icon_kari.js",
 ];
 
