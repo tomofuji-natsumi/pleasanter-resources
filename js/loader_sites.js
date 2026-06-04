a2fd42219b6a8bd00cd24b8c4d3a76c2d9e1a233
友藤なつみ <natsumi.tomofuji@proassist.co.jp>
2026-06-04 14:26:57 +0900

    Update and rename loader.js to loader_sites.js

--- js/loader_sites.js
+++ js/loader_sites.js
@@ -1,18 +1,20 @@
-// GitHub Pages に置いた複数 JS をまとめて読み込む
+// 複数JSをまとめて読み込む
 const scripts = [
   "https://tomofuji-natsumi.github.io/pleasanter-resources/js/port_setting_kari.js",
+  "https://tomofuji-natsumi.github.io/pleasanter-resources/js/sites/comment_color.js",
+  "https://tomofuji-natsumi.github.io/pleasanter-resources/js/sites/holiday_setting.js",
+  "https://tomofuji-natsumi.github.io/pleasanter-resources/js/sites/readonly.js",
   "https://tomofuji-natsumi.github.io/pleasanter-resources/js/icon_kari.js",
 ];
 
 function loadScriptSequential(urls) {
   return urls.reduce((p, url) => {
     return p.then(() => new Promise((resolve, reject) => {
-      // 既に読み込まれているならスキップ
       if (document.querySelector(`script[src="${url}"]`)) return resolve();
 
       const s = document.createElement('script');
       s.src = url;
-      s.async = false; // 順序保証
+      s.async = false;
       s.onload = resolve;
       s.onerror = reject;
       document.head.appendChild(s);
