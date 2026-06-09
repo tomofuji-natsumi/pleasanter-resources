de5c71059128c9246ab372d7ccc4893955a1a4e7
友藤なつみ <natsumi.tomofuji@proassist.co.jp>
2026-06-09 14:09:01 +0900

    Update back_btn2.js

--- js/folders/back_btn2.js
+++ js/folders/back_btn2.js
@@ -1,23 +1,30 @@
-(function () {
-
-  function replaceBackText() {
+function replaceBackText() {
     const title = document.querySelector("ul.nav-sites li.to-parent .title");
-    title.textContent = "戻る";
-      console.log("戻るに変更しました");
+    console.log("replaceBackText 実行", title);
+    if (title) {
+        title.textContent = "戻る";
+        console.log("→ 戻るに変更した");
     } else {
-      console.log("タイトルが見つからない");
+        console.log("→ title が見つからない");
     }
-  }
+}
 
- $(document).on("Common.Refresh", function () {
-    requestAnimationFrame(() => {
-      requestAnimationFrame(() => {
-        replaceBackText();
-      });
-    });
- 
-  document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOMContentLoaded 発火");
     replaceBackText();
-  });
+});
 
-})();
+$(document).on("pjax:end", () => {
+    console.log("pjax:end 発火");
+    replaceBackText();
+});
+
+$(document).on("pjax:success", () => {
+    console.log("pjax:success 発火");
+    replaceBackText();
+});
+
+$(document).on("Common.Refresh", () => {
+    console.log("Common.Refresh 発火");
+    replaceBackText();
+});
